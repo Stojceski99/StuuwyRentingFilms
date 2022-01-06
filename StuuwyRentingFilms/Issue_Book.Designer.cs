f706fe2b7399c2c6cb2426599eecd49545b4acfd
Hristijan Stojcheski <User@DESKTOP-27VNER1>
2022-01-06 01:29:35 +0100

    Added Issue Book for Administrators

---
+++ StuuwyRentingFilms/Issue_Book.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace StuuwyRentingFilms
+{
+    partial class Issue_Book
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelBackground = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxFirst = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxLast = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMovieTitle = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panelBackground.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelBackground
+            // 
+            this.panelBackground.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.panelBackground.Controls.Add(this.listBox1);
+            this.panelBackground.Controls.Add(this.label5);
+            this.panelBackground.Controls.Add(this.dateTimePicker1);
+            this.panelBackground.Controls.Add(this.textBoxMovieTitle);
+            this.panelBackground.Controls.Add(this.label4);
+            this.panelBackground.Controls.Add(this.textBoxLast);
+            this.panelBackground.Controls.Add(this.label2);
+            this.panelBackground.Controls.Add(this.button2);
+            this.panelBackground.Controls.Add(this.textBoxFirst);
+            this.panelBackground.Controls.Add(this.label3);
+            this.panelBackground.Controls.Add(this.label9);
+            this.panelBackground.Controls.Add(this.button1);
+            this.panelBackground.Controls.Add(this.textBox1);
+            this.panelBackground.Controls.Add(this.label1);
+            this.panelBackground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBackground.Location = new System.Drawing.Point(0, 0);
+            this.panelBackground.Name = "panelBackground";
+            this.panelBackground.Size = new System.Drawing.Size(566, 370);
+            this.panelBackground.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(250)))), ((int)(((byte)(90)))));
+            this.button1.Location = new System.Drawing.Point(12, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Search user";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 23);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(250)))), ((int)(((byte)(90)))));
+            this.label1.Location = new System.Drawing.Point(42, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Entre email:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(12, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 16);
+            this.label9.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(250)))), ((int)(((byte)(90)))));
+            this.button2.Location = new System.Drawing.Point(183, 318);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 33);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Issue Movie";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxFirst
+            // 
+            this.textBoxFirst.Location = new System.Drawing.Point(366, 35);
+            this.textBoxFirst.Name = "textBoxFirst";
+            this.textBoxFirst.Size = new System.Drawing.Size(170, 23);
+            this.textBoxFirst.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(250)))), ((int)(((byte)(90)))));
+            this.label3.Location = new System.Drawing.Point(262, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "First name";
+            // 
+            // textBoxLast
+            // 
+            this.textBoxLast.Location = new System.Drawing.Point(366, 75);
+            this.textBoxLast.Name = "textBoxLast";
+            this.textBoxLast.Size = new System.Drawing.Size(170, 23);
+            this.textBoxLast.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(250)))), ((int)(((byte)(90)))));
+            this.label2.Location = new System.Drawing.Point(262, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Last name";
+            // 
+            // textBoxMovieTitle
+            // 
+            this.textBoxMovieTitle.Location = new System.Drawing.Point(366, 170);
+            this.textBoxMovieTitle.Name = "textBoxMovieTitle";
+            this.textBoxMovieTitle.Size = new System.Drawing.Size(170, 23);
+            this.textBoxMovieTitle.TabIndex = 14;
+            this.textBoxMovieTitle.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxMovieTitle_KeyDown);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(250)))), ((int)(((byte)(90)))));
+            this.label4.Location = new System.Drawing.Point(262, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Movie title";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker1.CustomFormat = "dd,MM,yyyy,ddd";
+            this.dateTimePicker1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(366, 118);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(170, 25);
+            this.dateTimePicker1.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(250)))), ((int)(((byte)(90)))));
+            this.label5.Location = new System.Drawing.Point(262, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Issue date";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(366, 213);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(170, 93);
+            this.listBox1.TabIndex = 19;
+            this.listBox1.Visible = false;
+            this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
+            this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
+            // 
+            // Issue_Book
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 370);
+            this.Controls.Add(this.panelBackground);
+            this.Name = "Issue_Book";
+            this.Text = "Issue Book";
+            this.Load += new System.EventHandler(this.Issue_Book_Load);
+            this.panelBackground.ResumeLayout(false);
+            this.panelBackground.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelBackground;
+        private System.Windows.Forms.TextBox textBoxMovieTitle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxLast;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxFirst;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
---
+++ StuuwyRentingFilms/Issue_Book.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace StuuwyRentingFilms
+{
+    public partial class Issue_Book : Form
+    {
+        bool emailValidation = false;
+        readonly SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-27VNER1\TEW_SQLEXPRESS;Initial Catalog=movie_ManagmentSystem;Integrated Security=True"); // konekciski string do data bazata
+        public Issue_Book()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            String query = "SELECT * FROM movieUser WHERE email ='" + textBox1.Text + "'";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            i = Convert.ToInt32(dt.Rows.Count.ToString());
+
+            if (i == 0)
+            {
+                label9.Text = "Record not found";
+                MessageBox.Show("Record not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ClearBox();
+            }
+            else
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    textBoxFirst.Text = dr["firstName"].ToString();
+                    textBoxLast.Text = dr["lastName"].ToString();
+                }
+            }
+        }
+        private void textBoxMovieTitle_KeyDown(object sender, KeyEventArgs e)
+        {
+            listBox1.Focus();
+            //listBox1.SelectedIndex = 0;
+        }
+        private void listBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            textBoxMovieTitle.Text = listBox1.SelectedItem.ToString();
+            listBox1.Visible = false;
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBoxMovieTitle.Text = listBox1.SelectedItem.ToString();
+            listBox1.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int checkBookQuantity = 0;
+            checkBookQuantity = 0;
+            String queryCheck = "SELECT * FROM Movie WHERE movieTitle='" + textBoxMovieTitle.Text + "'";
+            SqlCommand cmdCheck = new SqlCommand(queryCheck, con);
+            cmdCheck.ExecuteNonQuery();
+            DataTable dtCheck = new DataTable();
+            SqlDataAdapter daCheck = new SqlDataAdapter(cmdCheck);
+            daCheck.Fill(dtCheck);
+
+            foreach (DataRow drCheck in dtCheck.Rows)
+            {
+                checkBookQuantity = Convert.ToInt32(drCheck["movieAvailableQuantity"].ToString());
+            }
+
+            if (checkBookQuantity > 0)
+            {
+                // Query for inserting information about issued movie in database
+                String query = "INSERT INTO MovieIssue VALUES(" + textBoxMovieTitle.Text + ",'" + textBox1.Text + "','" + dateTimePicker1.Value.ToShortDateString() + "','')";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+
+                // Query for updating availableQuantity
+                String queryUpdate = "UPDATE Movie SET movieAvailableQuantity = movieAvailableQuantity-1 WHERE movieTitle ='" + textBoxMovieTitle.Text + "' ";
+                SqlCommand cmdUpdate = new SqlCommand(queryUpdate, con);
+                cmdUpdate.ExecuteNonQuery();
+
+                ClearBox();
+                MessageBox.Show("Movie issued successfully.", "Inforamation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                textBoxMovieTitle.Focus();
+                MessageBox.Show("Movie not available.", "Inforamation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void Issue_Book_Load(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+        }
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            emailValidation = ValidateEmail(textBox1, label1);
+        }
+        //METODI
+        private void ClearBox()
+        {
+            textBox1.Text = "";
+            textBoxFirst.Text = "";
+            textBoxLast.Text = "";
+            textBoxMovieTitle.Text = "";
+        }
+        private int CheckAvailability(int checkMovieQuantity)
+        {
+            checkMovieQuantity = 0;
+            String queryCheck = "SELECT * FROM Movie WHERE movieTitle='" + textBoxMovieTitle.Text + "'";
+            SqlCommand cmdCheck = new SqlCommand(queryCheck, con);
+            cmdCheck.ExecuteNonQuery();
+            DataTable dtCheck = new DataTable();
+            SqlDataAdapter daCheck = new SqlDataAdapter(cmdCheck);
+            daCheck.Fill(dtCheck);
+            foreach (DataRow drCheck in dtCheck.Rows)
+            {
+                checkMovieQuantity = Convert.ToInt32(drCheck["movieAvailableQuantity"].ToString());
+            }
+            return checkMovieQuantity;
+        }
+        public bool ValidateEmail(TextBox textBox, Label label)
+        {
+            if (!Regex.Match(textBox.Text, "^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$").Success)
+            {
+                label1.Text = label.Text + " is invalid.";
+                MessageBox.Show("Invalid " + label.Text, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Focus();
+                textBox.Text = "";
+                return false;
+            }
+            else
+                return true;
+        }
+    }
+}
